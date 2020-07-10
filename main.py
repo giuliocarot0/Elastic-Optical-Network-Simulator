6a9ba0b7710e134613866093c59bd2b9e0bc00c8
giuliocarot0 <giulio.carota@outlook.it>
2020-07-10 15:30:31 +0200

    3D Histograms + Traffic Matrix

--- main.py
+++ main.py
@@ -1,34 +1,79 @@
 import numpy as np
+import pandas as pd
+import itertools as it
 from libs.conenction import Connection
 from libs.network import Network
 from random import shuffle
 import matplotlib.pyplot as plt
 
-network = Network('nodes.json')
-network.connect()
-network.show_topology()
+
+def create_traffic_matrix(nodes, rate):
+    s = pd.Series(data=[0.0] * len(nodes), index=nodes)
+    df = pd.DataFrame(float(rate), index=s.index, columns=s.index, dtype=s.dtype)
+    np.fill_diagonal(df.values, s)
+    return df
 
 
-nodes = list(network.nodes.keys())
-connections = []
-connections_occupied = []
+def plot3dbars(t):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x, y = np.meshgrid(np.arange(t.shape[1]), np.arange(t.shape[0]))
+    x = x.flatten()
+    y = y.flatten()
+    z = t.flatten()
+    ax.bar3d(x, y, np.zeros(len(z)), 1, 1, z)
+    plt.show()
 
-for i in range(100):
-    shuffle(nodes)
-    connection = Connection(nodes[0], nodes[-1])
-    connections.append(connection)
 
-streamed2 = network.stream(connections, 'snr', "occupied")
-snr_o = [connection.snr for connection in streamed2]
-if 0 in snr_o:
-    print("Conenction blocked")
-plt.hist(snr_o)
-plt.title("SNRs distribution w/ occupied ")
-plt.show()
+def main():
+    network = Network('nodes.json')
+    network.connect()
+    network.show_topology()
 
-rbs = [connection.calculate_capacity() for connection in streamed2]
-plt.hist(rbs)
-plt.title('Bitrate Distribution [ Gbps ]')
-plt.show()
-print('Total Capacity : {:.2f} Tbps'.format(np.sum(rbs)*1e-3))
-print('Avg Capacity : {:.2f} Gbps'.format(np.mean(rbs)))
+    nodes = list(network.nodes.keys())
+    T = create_traffic_matrix(nodes, 1200)
+    t = T.values
+
+    connections = []
+    node_pairs = list(filter(lambda x: x[0] != x[1], list(it.product(nodes, nodes))))
+    shuffle(node_pairs)
+
+    for node_pair in node_pairs:
+        connection = Connection(node_pair[0], node_pair[1], float(T.loc[node_pair[0], node_pair[-1]]))
+        connections.append(connection)
+
+    streamed = network.stream(connections, best='snr', transceiver='flex-rate')
+    snrs = []
+    [snrs.extend(connection.snr) for connection in streamed]
+
+    rbl = []
+    for connection in streamed:
+        for lightpath in connection.lightpaths:
+            rbl.append(lightpath.bitrate)
+
+    plt.hist(snrs)
+    plt.title("SNRs distribution w/ occupied ")
+    plt.show()
+
+    rbc = [connection.calculate_capacity() for connection in streamed]
+    plt.hist(rbc)
+    plt.title('Connection Bitrate Distribution [ Gbps ]')
+    plt.show()
+
+    plt.hist(rbl)
+    plt.title('Lightpath Bitrate Distribution [ Gbps ]')
+    plt.show()
+
+    s = pd.Series(data=[0.0] * len(nodes), index=nodes)
+    df = pd.DataFrame(0.0, index=s.index, columns=s.index, dtype=s.dtype)
+    print(df)
+    for connection in streamed:
+        df.loc[connection.input_node, connection.output_node] = connection.bitrate
+
+    print(df)
+    plot3dbars(t)
+    plot3dbars(df.values)
+
+
+if __name__ == '__main__':
+    main()
